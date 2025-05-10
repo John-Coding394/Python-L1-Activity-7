ac7af98f5be94009819b19d9c8c2f01395441f1f
John-Coding394 <johnchaghouri394@gmail.com>
2025-05-10 10:28:30 -0400

    First commit

---
+++ bmi.py
@@ -0,0 +1,24 @@
+h = float(input("Enter your height in cm: "))
+w = float(input("Enter your weight in kg: "))
+
+BMI = w / (h/100)**2
+
+print("Your BMI is ", BMI)
+
+if BMI <=18.4:
+    print("Your are underweight")
+
+elif BMI <= 24.9:
+    print("You are healthy")
+
+elif BMI <=29.9:
+    print("You are overweight")
+
+elif BMI <= 34.9:
+    print("You are severely overweight")
+
+elif BMI <= 39.9:
+    print("You are obese")
+
+else: 
+    print("You are severely obese")
---
+++ main.py
@@ -0,0 +1,26 @@
+a = int(input())
+b = int(input())
+if a>50 and b<90:
+    print("True")
+else:
+    print("False")
+
+if a == b:
+    print("They are equal")
+
+else:
+    print("They are not equal")
+
+if a != b: 
+    print("They are not equal")
+
+else: 
+    print("They are equal")
+
+if a==50 or b>30:
+    print("Ok condition true")
+
+else:
+    print("Ok condition false")
+
+
